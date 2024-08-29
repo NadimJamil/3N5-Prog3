fa2e30f44eee16b293cb9c122d440e67c22bcbbd
Nadim Jamil <2261561@cegepmontpetit.ca>
2024-08-29 12:57:38 -0400

    Commit continuation

--- code/Types-Cour2.0-2.1/src/main/kotlin/moyenne.kt
+++ code/Types-Cour2.0-2.1/src/main/kotlin/moyenne.kt
@@ -1,15 +1,22 @@
 import kotlin.random.Random
 
 fun main(){
-creeTableauAleatoire(3)
+creeTableauAleatoire(5)
 }
 
 fun creeTableauAleatoire(n: Int): Array<Int> {
     val randomValues = List(n) { Random.nextInt(0, 100) }
     println(randomValues)
 
-    val nextValues = List(n) { Random.nextInt(0, 100) }
-    println(nextValues)
-    println("randomValues != nextValues is ${randomValues != nextValues}")
-    return randomValues
+    var array = arrayOf(n)
+    return array
 }
+
+
+
+//fun moyenne() : Double{
+//    var sum: Double = 0.0
+//    var count: Int = 0
+//    for (element in )
+//}
+
