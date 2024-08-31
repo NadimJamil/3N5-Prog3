424fc291ef4a7d9fa5f24f55d37163f7f1c25305
Nadim <2261561@cegepmontpetit.ca>
2024-08-31 14:07:42 -0400

    Commit exercice moyenne
    Fin exercice moyenne

--- code/Types-Cour2.0-2.1/src/main/kotlin/moyenne.kt
+++ code/Types-Cour2.0-2.1/src/main/kotlin/moyenne.kt
@@ -1,22 +1,42 @@
 import kotlin.random.Random
 
 fun main(){
-creeTableauAleatoire(5)
+    var liste = creeTableauAleatoire((6))
+    somme( liste)
+    moyenne(liste)
+    min(liste)
+    max(liste)
 }
 
 fun creeTableauAleatoire(n: Int): Array<Int> {
     val randomValues = List(n) { Random.nextInt(0, 100) }
     println(randomValues)
 
-    var array = arrayOf(n)
+    var array = randomValues.toTypedArray()
     return array
 }
 
+fun somme(list : Array<Int>) : Int{
+    var sumList = list.sum()
+    println(sumList)
+    return sumList
+}
 
+fun moyenne(list : Array<Int>) : Double{
+    var avgList = list.average()
+    println(avgList)
+    return avgList
+}
 
-//fun moyenne() : Double{
-//    var sum: Double = 0.0
-//    var count: Int = 0
-//    for (element in )
-//}
+fun min(list : Array<Int>) : Int{
+    var minList = list.min()
+    println(minList)
+    return minList
+}
+
+fun max(list : Array<Int>) : Int{
+    var maxList = list.max()
+    println(maxList)
+    return maxList
+}
 
